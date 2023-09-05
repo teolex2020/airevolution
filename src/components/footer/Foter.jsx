a7b7d20e92b9dcba7d3a7d73cb713c4d6b39daa9
Olexsandr Tepliuk <teolex2017@gmail.com>
2023-09-05 11:04:20 +0300

    social media

--- src/components/footer/Foter.jsx
+++ src/components/footer/Foter.jsx
@@ -3,35 +3,38 @@ import Image from 'next/image'
 
 const Footer = () => {
 	return (
-		<div className='py-4 text-center flex justify-between'>
+		<div className='py-4 flex justify-between '>
 			<div>©2023 AI-revolution.</div>
-			<div className='flex space-x-6'>
-				<a href='https://www.facebook.com/O.Teplyk'>
-					<div className='hidden md:flex flex-1 relative w-6 h-6'>
-						<Image
-							src='/Facebook.svg'
-							alt=''
-							fill
-							priority
-							className='object-contain '
-							sizes='(min-width: 500px) 50vw, 100vw'
-						/>
-					</div>
-				</a>
+			<div className='flex flex-row space-x-4 '>
+		
+		
+					<a href='https://www.facebook.com/O.Teplyk' >
+						<div className=' relative w-6 h-6'>
+							<Image
+								src='/Facebook.svg'
+								alt=''
+								fill
+								priority
+								className='object-contain '
+							/>
+						</div>
+					</a>
+			
+
 				<a href='https://www.instagram.com/newtom1983/'>
-					<div className='hidden md:flex flex-1 relative w-6 h-6'>
+					<div className='relative w-6 h-6'>
 						<Image
 							src='/Instagram.svg'
 							alt=''
 							fill
 							priority
 							className='object-contain '
-							sizes='(min-width: 500px) 50vw, 100vw'
+							sizes='(min-width: 20px) 50vw, 100vw'
 						/>
 					</div>
 				</a>
 				<a href='https://www.tiktok.com/@alexai2023?lang=en'>
-					<div className='hidden md:flex flex-1 relative w-6 h-6'>
+					<div className=' relative w-6 h-6'>
 						<Image
 							src='/TikTok.svg'
 							alt=''
