adc600c7d5df0d54adcfa9cca97a767c8b8dc023
Olexsandr Tepliuk <teolex2017@gmail.com>
2024-01-03 13:32:39 +0200

    logout

--- src/app/components/Navbar/Signin.jsx
+++ src/app/components/Navbar/Signin.jsx
@@ -3,7 +3,7 @@ import { Gentium_Book_Plus } from 'next/font/google'
 import { useSelector, useDispatch } from 'react-redux'
 import { useRouter } from 'next/navigation'
 import { getAuth, signOut } from 'firebase/auth'
-import {Authuser} from "../../../store/features/counterSlice"
+import { Authuser, MobileMenus } from '../../../store/features/counterSlice'
 
 const gentium = Gentium_Book_Plus({
 	weight: '400',
@@ -14,44 +14,46 @@ const Signin = () => {
 	const dispatch = useDispatch()
 	const router = useRouter()
 	const authuser = useSelector((state) => state.counter.authUser) || ''
-
-
+	const mobileMenu = useSelector((state) => state.counter.mobileMenu)
 
 	const singout = () => {
 		const auth = getAuth()
 		signOut(auth)
 			.then(() => {
 				dispatch(Authuser(false))
-				router.push('/login')
+				dispatch(MobileMenus(mobileMenu))
+			
 			})
 			.catch((error) => {
 				// An error happened.
 			})
 	}
 
-	
+	const singin = () => {
+		
+		router.push('/login')
+		dispatch(MobileMenus(mobileMenu))
+	}
 
 	return (
 		<div>
-		{authuser? 
+			{authuser ? (
 				<button
 					onClick={() => singout()}
 					className={` border-2 rounded-3xl border-zinc-700/50 px-10 py-2 hover:font-semibold justify-center  flex    duration-300  z-10   items-center gap-5  uppercase  ${gentium.className} text-xl`}
 				>
 					Вийти
 				</button>
-			 :
-			<button
-				onClick={() => router.push('/login')}
-				className={`border-2 rounded-3xl border-zinc-700/50 px-10 py-2 hover:font-semibold justify-center flex    duration-300  z-10   items-center gap-5  uppercase  ${gentium.className} text-xl`}
-			>
-				Увійти
-			</button>
-
-		
-
-}
-	</div>)
+			) : (
+				<button
+					onClick={() => singin()}
+					className={`border-2 rounded-3xl border-zinc-700/50 px-10 py-2 hover:font-semibold justify-center flex    duration-300  z-10   items-center gap-5  uppercase  ${gentium.className} text-xl`}
+				>
+					Увійти
+				</button>
+			)}
+		</div>
+	)
 }
 
 export default Signin
