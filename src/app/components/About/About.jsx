68d0802a23d146963fa268e46c571cef607783a7
Olexsandr Tepliuk <teolex2017@gmail.com>
2024-05-31 15:19:54 +0300

    upgrate

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -16,61 +16,68 @@ const About = () => {
   return (
 		<div className='flex justify-center items-center mx-auto flex-col gap-5  px-5 container max-w-7xl '>
 			<div className=' flex flex-col md:flex-row w-fit gap-3   justify-center relative items-center'>
-				<div className='flex-col w-64 h-80 p-4 rounded-lg  flex justify-center items-center animation'>
-					<div className='w-48 h-48 rounded-lg bg-blur animation relative'>
-						<Image
-							src='/alex.webp'
-							fill
-							alt='Picture of the author'
-							sizes='(min-width: 808px) 50vw, 100vw'
-							className='object-cover rounded-lg'
-							priority
-						/>
+				<a href='https://www.linkedin.com/in/alexander-tepliuk-448485161/'>
+					{' '}
+					<div className='flex-col w-64 h-80 p-4 rounded-lg  flex justify-center items-center animation cursor-pointer'>
+						<div className='w-48 h-48 rounded-lg bg-blur animation relative'>
+							<Image
+								src='/alex.webp'
+								fill
+								alt='Picture of the author'
+								sizes='(min-width: 808px) 50vw, 100vw'
+								className='object-cover rounded-lg'
+								priority
+							/>
+						</div>
+						<div className='text-center pt-2'>
+							<span className='text-lg'>Oleksandr Tepliuk</span> <br />
+							<span className='text-[11px]'>
+								Artificial Intelligence Ambassador, Artificial Intelligence
+								Integration Consultant for Your Business
+							</span>
+						</div>
 					</div>
-					<div className='text-center pt-2'>
-						<span className='text-lg'>Oleksandr Tepliuk</span> <br />
-						<span className='text-[11px]'>
-							Artificial Intelligence Ambassador, Artificial Intelligence
-							Integration Consultant for Your Business
-						</span>
+				</a>
+				<a href='https://www.linkedin.com/in/maximlevitskiy/'>
+					<div className='flex-col w-64 h-80 p-4 rounded-lg  flex justify-center items-center animation cursor-pointer'>
+						<div className='w-48 h-48 rounded-lg bg-blur animation relative'>
+							<Image
+								src='/max.webp'
+								fill
+								alt='Picture of the author'
+								sizes='(min-width: 808px) 50vw, 100vw'
+								className='object-cover rounded-lg'
+								priority
+							/>
+						</div>
+						<div className='text-center pt-2'>
+							<span className='text-lg'>Maxim Levitskiy</span> <br />
+							<span className='text-[11px]'>
+								GenAI, Leadership, Clouds, Blockchain, Programming, Web Tree
+							</span>
+						</div>
 					</div>
-				</div>
-				<div className='flex-col w-64 h-80 p-4 rounded-lg  flex justify-center items-center animation'>
-					<div className='w-48 h-48 rounded-lg bg-blur animation relative'>
-						<Image
-							src='/max.webp'
-							fill
-							alt='Picture of the author'
-							sizes='(min-width: 808px) 50vw, 100vw'
-							className='object-cover rounded-lg'
-							priority
-						/>
+				</a>
+				<a href='https://www.linkedin.com/in/alex-zaichenko/'>
+					<div className='flex-col w-64 h-80 p-4 rounded-lg  flex justify-center items-center animation cursor-pointer'>
+						<div className='w-48 h-48 rounded-lg bg-blur animation relative'>
+							<Image
+								src='/olex.webp'
+								fill
+								alt='Picture of the author'
+								sizes='(min-width: 808px) 50vw, 100vw'
+								className='object-cover rounded-lg'
+								priority
+							/>
+						</div>
+						<div className='text-center pt-2'>
+							<span className='text-lg'>Alex Zaichenko</span> <br />
+							<span className='text-[11px]'>
+								Program, Manager, CSM ,AI enthusiast, AI Podcast, IT Volunteer
+							</span>
+						</div>
 					</div>
-					<div className='text-center pt-2'>
-						<span className='text-lg'>Maxim Levitskiy</span> <br />
-						<span className='text-[11px]'>
-							GenAI, Leadership, Clouds, Blockchain, Programming, Web Tree
-						</span>
-					</div>
-				</div>
-				<div className='flex-col w-64 h-80 p-4 rounded-lg  flex justify-center items-center animation'>
-					<div className='w-48 h-48 rounded-lg bg-blur animation relative'>
-						<Image
-							src='/olex.webp'
-							fill
-							alt='Picture of the author'
-							sizes='(min-width: 808px) 50vw, 100vw'
-							className='object-cover rounded-lg'
-							priority
-						/>
-					</div>
-					<div className='text-center pt-2'>
-						<span className='text-lg'>Alex Zaichenko</span> <br />
-						<span className='text-[11px]'>
-							Program, Manager, CSM ,AI enthusiast, AI Podcast, IT Volunteer
-						</span>
-					</div>
-				</div>
+				</a>
 			</div>
 
 			{data.map((e) => (
